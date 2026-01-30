eff0504d99d6e10dd741e177f9082037c61f42fc
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-30 16:01:53 -0500

    Refactor test.

--- sims/tests.py
+++ sims/tests.py
@@ -5,8 +5,7 @@ from sims.views import home_page
 class HomePageTest(TestCase):
   def test_home_page_returns_correct_html(self):
     response = self.client.get('/')
-    html = response.content.decode('utf8')
-    self.assertIn('<title>Risk Simulation</title>', html)
-    self.assertTrue(html.startswith('<html>'))
-    self.assertTrue(html.endswith('</html>'))
+    self.assertContains(response, '<title>Risk Simulation</title>')
+    self.assertContains(response, '<html>')
+    self.assertContains(response, '</html>')
 
