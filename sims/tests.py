80b8bf50008903cd392f7fbe5ddc01a0fc86e156
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-30 15:55:36 -0500

    First unit test passes.

--- sims/tests.py
+++ sims/tests.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from sims.views import home_page
 
-class SmokeTest(TestCase):
-    def test_basic_addition(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+  def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    html = response.content.decode('utf8')
+    self.assertIn('<title>Risk Simulation</title>', html)
+    self.assertTrue(html.startswith('<html>'))
+    self.assertTrue(html.endswith('</html>'))
