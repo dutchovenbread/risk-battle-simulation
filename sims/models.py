19a08ae5a8e7dbd619ee8318b723b9666c84137d
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-03-12 10:58:06 -0400

    Initial model version.

--- sims/models.py
+++ sims/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 class Simulation(models.Model):
-  pass
-  # attacking_armies_starting = models.IntegerField()
-  # defending_armies_starting = models.IntegerField()
-  # attacking_armies_remaining = models.IntegerField()
-  # defending_armies_remaining = models.IntegerField()
-  # result_statement = models.CharField(max_length=20)
+  attacking_armies_starting = models.IntegerField()
+  defending_armies_starting = models.IntegerField()
+  attacking_armies_remaining = models.IntegerField()
+  defending_armies_remaining = models.IntegerField()
+  result_statement = models.CharField(max_length=20)
